e1c6c084d5251a59bfda66ff07d52a6021a30078
Den106 <eaglemen12@gmail.com>
2022-10-05 11:34:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-def greet():
+def salut():
     print("-------------------")
     print("  Приветсвуем вас  ")
     print("      в игре       ")
     print("  крестики-нолики  ")
     print("-------------------")
-    print(" формат ввода: x y ")
-    print(" x - номер строки  ")
-    print(" y - номер столбца ")
 
 def show():
     print()
@@ -16,17 +13,20 @@ def show():
         row_str = f"  {i} | {' | '.join(row)} | "
         print(row_str)
         print("  --------------- ")
+    print(" формат ввода: x y ")
+    print(" x - номер строки  ")
+    print(" y - номер столбца ")
     print()
 
 def ask():
     while True:
-        cords = input("         Ваш ход: ").split()
+        moves = input("    Ваш ход: ").split()
         
-        if len(cords) != 2:
+        if len(moves) != 2:
             print(" Введите 2 координаты! ")
             continue
         
-        x, y = cords
+        x, y = moves
         
         if not(x.isdigit()) or not(y.isdigit()):
             print(" Введите числа! ")
@@ -34,23 +34,23 @@ def ask():
         
         x, y = int(x), int(y)
         
-        if 0 > x or x > 2 or  0 > y or  y > 2 :
-            print(" Координаты вне диапазона! ")
-            continue
-        
         if field[x][y] != " ":
             print(" Клетка занята! ")
+            continue
+        
+        if 0 > x or x > 2 or  0 > y or  y > 2 :
+            print(" Координаты вне диапазона! ")
             continue
         
         return x, y
             
 def check_win():
-    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+    win_move = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
                 ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
                 ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
-    for cord in win_cord:
+    for move in win_move:
         symbols = []
-        for c in cord:
+        for c in move:
             symbols.append(field[c[0]][c[1]])
         if symbols == ["X", "X", "X"]:
             print("Выиграл X!!!")
@@ -60,7 +60,7 @@ def check_win():
             return True
     return False
 
-greet()
+salut()
 field = [[" "] * 3 for i in range(3) ]
 count = 0
 while True:
